3f587509654cebd000e9b8e60cb9cb053d7af3e4
karoll1naa <karollinnajan@gmail.com>
2024-06-05 16:08:59 +0300

    додала рахунок

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -14,6 +14,7 @@ public class Game {
     public static boolean moving = false, hasMoved = true, somethingIsMoving = false;
     public static int direction = 0;
     private final Random rand = new Random();
+    private int score;
     public Game() {
         init();
     }
@@ -22,6 +23,7 @@ public class Game {
         moving = false;
         hasMoved = true;
         somethingIsMoving = false;
+        score = 0;
         spawn();
 
     }
@@ -44,6 +46,7 @@ public class Game {
                 if (objects.get(i).x == objects.get(j).x && objects.get(i).y == objects.get(j).y && !objects.get(i).remove && !objects.get(j).remove) {
                     objects.get(j).remove = true;
                     objects.get(i).value *= 2;
+                    score += objects.get(i).value;
                     objects.get(i).createSprite();
                 }
             }
@@ -119,6 +122,13 @@ public class Game {
             Main.pixels[i] = Renderer.pixels[i];
         }
     }
+    public void renderScore(Graphics2D g) {
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.setFont(new Font("Verdana", Font.BOLD, 24));
+        g.setColor(Color.BLACK);
+        g.drawString("Score: " + score, 10, 40);
+    }
+
     public void renderText(Graphics2D g) {
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g.setFont(new Font("Verdana", 0, 100));
@@ -130,5 +140,7 @@ public class Game {
             int sw = (int) (g.getFontMetrics().stringWidth(s) / 2 / Main.scale);
             g.drawString(s, (int) (o.x + o.width / 2 - sw) * Main.scale, (int) (o.y + o.height / 2 + 18) * Main.scale);
         }
+        renderScore(g);
     }
+
 }
