6ed4659b12495491f584d84b9f8fb289e9d87249
karoll1naa <karollinnajan@gmail.com>
2024-06-06 18:37:26 +0300

    тест метода canMove

---
+++ src/Tests/canMoveTest.java
@@ -0,0 +1,41 @@
+package Tests;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Main.Game;
+import Main.GameObject;
+
+public class canMoveTest {
+    private Game game;
+
+    @Before
+    public void setUp() {
+        game = new Game();
+    }
+
+    @Test
+    public void testCanMoveWhenNoCollisions() {
+        GameObject obj = new GameObject(50, 100);
+        Game.objects.clear();
+        Game.objects.add(obj);
+
+        boolean canMove = obj.canMove();
+        assertTrue(canMove);
+    }
+
+    @Test
+    public void testCanMoveWhenCollisions() {
+        GameObject obj1 = new GameObject(50, 50);
+        GameObject obj2 = new GameObject(90, 90);
+        Game.objects.clear();
+        Game.objects.add(obj1);
+        Game.objects.add(obj2);
+
+        boolean canMove = obj1.canMove();
+
+        assertFalse(canMove);
+    }
+}
